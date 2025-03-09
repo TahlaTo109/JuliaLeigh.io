2b26af5838743417289ed3c70c92e4180e963419
Julia <198022687+TahlaTo109@users.noreply.github.com>
2025-03-08 23:36:38 -0600

    Added initial Website Files

---
+++ js/defunct.js
@@ -0,0 +1,32 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+});
+
+
+function searchURL(){
+    console.log(searchTxt.value);
+    // window.location = "http://www.myurl.com/search/" + searchTxt.value;
+    document.getElementById("demo").innerHTML = 
+    searchTxt.value;
+  }
+
+
+
+
+
